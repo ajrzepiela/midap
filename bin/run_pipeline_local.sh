d3273085ef7f966fb7b36078fcf1db2aae47513c
oschmanf <franziska.oschmann@id.ethz.ch>
2021-07-23 13:37:10 +0200

    Reset start and end frame for tracking for each position and channel
    Former-commit-id: f22a4edf7b417a37384447bfe38bd0f85f121d58

--- bin/run_pipeline_local.sh
+++ bin/run_pipeline_local.sh
@@ -27,181 +27,191 @@ if [[ $DATA_TYPE == "FAMILY_MACHINE" ]]
         POS_UNIQ=($(printf "%s\n" "${POSITIONS[@]}" | sort -u));
 
         for POS in "${POS_UNIQ[@]}"; do
-        echo $POS
+                echo $POS
 
-        # specify different folders needed for segmentation and tracking
-        RAW_IM="raw_im/"
-        SEG_PATH="xy1/"
-        CUT_PATH="phase/"
-        SEG_IM_PATH="seg_im/"
-        SEG_MAT_PATH="seg/"
-        SEG_IM_TRACK_PATH="input_ilastik_tracking/"
-
-        # 1) Generate folder structure
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
-        then
-                echo "generate folder structure"
-
-                # Delete results folder for this position in case it already exists.
-                # In this way the segmentation can be rerun
-                delete_folder $PATH_FOLDER$POS
-
-                # generate folders for different channels (phase, fluorescent)
-                make_dir $PATH_FOLDER$POS
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/
-                done
-
-                # generate folder raw_im for raw images
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$RAW_IM
-                done
-
-                # generate folder for tracking results
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH
-                done
-
-                # generate folder for cutout images
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH$CUT_PATH
-                done
-
-                # generate folder seg_im for segmentation images
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH
-                done
-
-                # generate folder seg_im_track for stacks of segmentation images for tracking
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$SEG_IM_TRACK_PATH
-                done
-
-                # generate folders for segmentation-mat files
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH
-                done
-        fi
-
-        # 2) Copy files
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                # restrict frames for each position separately
+                if  [ $POS != "${POS_UNIQ[0]}" ]
                 then
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        VAR=`find $PATH_FOLDER -name *$POS*${!CH}*.$FILE_TYPE`
-                        cp $VAR $PATH_FOLDER$POS/${!CH}/
-                done
-        fi
-
-
-        # 3) Split frames
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
-        then
-                echo "split frames"
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        INP=$(find $PATH_FOLDER$POS/${!CH}/ -name *.$FILE_TYPE)
-                        python stack2frames.py --path $INP --pos $POS --channel /${!CH}/ --start_frame $START_FRAME --end_frame $END_FRAME --deconv $DECONVOLUTION
-                done
-        fi
-
-
-        # 4) Cut chambers
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
-        then
-                echo "cut chambers"
-                if [ -z "$CHANNEL_2" ] || [ -z "$CHANNEL_3" ]
-                        then
-                        python frames2cuts.py --path_ch0 $PATH_FOLDER$POS/$CHANNEL_1/$RAW_IM
-                        echo $PATH_FOLDER$POS$CHANNEL_1$RAW_IM
-                else
-                        python frames2cuts.py --path_ch0 $PATH_FOLDER$POS/$CHANNEL_1/$RAW_IM --path_ch1 $PATH_FOLDER$POS/$CHANNEL_2/$RAW_IM --path_ch2 $PATH_FOLDER$POS/$CHANNEL_3/$RAW_IM
+                        echo $POS
+                        python restrict_frames.py
+                        source settings.sh
                 fi
-        fi
 
+                # specify different folders needed for segmentation and tracking
+                RAW_IM="raw_im/"
+                SEG_PATH="xy1/"
+                CUT_PATH="phase/"
+                SEG_IM_PATH="seg_im/"
+                SEG_MAT_PATH="seg/"
+                SEG_IM_TRACK_PATH="input_ilastik_tracking/"
 
-        # 5) Segmentation
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
-        then
-                echo "segment images"
-                if [ "$PHASE_SEGMENTATION" == True ]
-                        then 
+                # 1) Generate folder structure
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                then
+                        echo "generate folder structure"
+
+                        # Delete results folder for this position in case it already exists.
+                        # In this way the segmentation can be rerun
+                        delete_folder $PATH_FOLDER$POS
+
+                        # generate folders for different channels (phase, fluorescent)
+                        make_dir $PATH_FOLDER$POS
                         for i in $(seq 1 $NUM_CHANNEL_TYPES); do
                                 CH="CHANNEL_$i"
-                                python main_prediction.py --path_model_weights '../model_weights/model_weights_family_mother_machine/' --path_pos $PATH_FOLDER$POS --path_channel ${!CH} --postprocessing 1
-                                python analyse_segmentation.py --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH/ --path_result $PATH_FOLDER$POS/${!CH}/
+                                make_dir $PATH_FOLDER$POS/${!CH}/
                         done
-                elif [ "$PHASE_SEGMENTATION" == False ]
-                        then
-                        for i in $(seq 2 $NUM_CHANNEL_TYPES); do
+
+                        # generate folder raw_im for raw images
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
                                 CH="CHANNEL_$i"
-                                python main_prediction.py --path_model_weights '../model_weights/model_weights_family_mother_machine/' --path_pos $PATH_FOLDER$POS --path_channel ${!CH} --postprocessing 1
-                                python analyse_segmentation.py --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH/ --path_result $PATH_FOLDER$POS/${!CH}/
+                                make_dir $PATH_FOLDER$POS/${!CH}/$RAW_IM
+                        done
+
+                        # generate folder for tracking results
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH
+                        done
+
+                        # generate folder for cutout images
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH$CUT_PATH
+                        done
+
+                        # generate folder seg_im for segmentation images
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                make_dir $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH
+                        done
+
+                        # generate folder seg_im_track for stacks of segmentation images for tracking
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                make_dir $PATH_FOLDER$POS/${!CH}/$SEG_IM_TRACK_PATH
+                        done
+
+                        # generate folders for segmentation-mat files
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                make_dir $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH
                         done
                 fi
-        fi
-
-        # 6) Conversion
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
-        then
-                echo "run file-conversion"
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        python seg2mat.py --path_cut $PATH_FOLDER$POS/${!CH}/$SEG_PATH$CUT_PATH --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH --path_channel $PATH_FOLDER$POS/${!CH}/
-                done
-        fi
 
 
-        # 7) Tracking
-        if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "TRACKING" ]]
-        then
-                echo "run tracking"
-                
-                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
-                        CH="CHANNEL_$i"
-                        echo ${!CH}
+                # 2) Copy files
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                        then
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                VAR=`find $PATH_FOLDER -name *$POS*${!CH}*.$FILE_TYPE`
+                                cp $VAR $PATH_FOLDER$POS/${!CH}/
+                        done
+                fi
 
-                        # delete all files related to SuperSegger to ensure that SuperSegger runs
-                        rm $PATH_FOLDER$POS/${!CH}/CONST.mat
-                        rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH/clist.mat
-                        rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH/*_err.mat
-                        rm -r $PATH_FOLDER$POS/${!CH}/$SEG_PATH/cell
-                        rm $PATH_FOLDER$POS/${!CH}/$RAW_IM/cropbox.mat
 
-                        $MATLAB_ROOT/bin/matlab -nodisplay -r "tracking_supersegger('$PATH_FOLDER$POS/${!CH}/', '$CONSTANTS' , $NEIGHBOR_FLAG, $TIME_STEP, $MIN_CELL_AGE, '$DATA_TYPE')"
-                        MAT_FILE=$PATH_FOLDER$POS/${!CH}/$SEG_PATH/clist.mat
-                        echo $MAT_FILE
+                # 3) Split frames
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                then
+                        echo "split frames"
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                INP=$(find $PATH_FOLDER$POS/${!CH}/ -name *.$FILE_TYPE)
+                                python stack2frames.py --path $INP --pos $POS --channel /${!CH}/ --start_frame $START_FRAME --end_frame $END_FRAME --deconv $DECONVOLUTION
+                        done
+                fi
 
-                        # as long as 'clist.mat' is missing (hint for failed SuperSegger) the tracking can be repeated with a reduced number of frames
-                        while ! test -f "$MAT_FILE"; do
-                                #rm -r $PATH_FOLDER$POS/${!CH}/$SEG_PATH/
+
+                # 4) Cut chambers
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                then
+                        echo "cut chambers"
+                        if [ -z "$CHANNEL_2" ] || [ -z "$CHANNEL_3" ]
+                                then
+                                python frames2cuts.py --path_ch0 $PATH_FOLDER$POS/$CHANNEL_1/$RAW_IM
+                                echo $PATH_FOLDER$POS$CHANNEL_1$RAW_IM
+                        else
+                                python frames2cuts.py --path_ch0 $PATH_FOLDER$POS/$CHANNEL_1/$RAW_IM --path_ch1 $PATH_FOLDER$POS/$CHANNEL_2/$RAW_IM --path_ch2 $PATH_FOLDER$POS/$CHANNEL_3/$RAW_IM
+                        fi
+                fi
+
+
+                # 5) Segmentation
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                then
+                        echo "segment images"
+                        if [ "$PHASE_SEGMENTATION" == True ]
+                                then 
+                                for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                        CH="CHANNEL_$i"
+                                        python main_prediction.py --path_model_weights '../model_weights/model_weights_family_mother_machine/' --path_pos $PATH_FOLDER$POS --path_channel ${!CH} --postprocessing 1
+                                        python analyse_segmentation.py --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH/ --path_result $PATH_FOLDER$POS/${!CH}/
+                                done
+                        elif [ "$PHASE_SEGMENTATION" == False ]
+                                then
+                                for i in $(seq 2 $NUM_CHANNEL_TYPES); do
+                                        CH="CHANNEL_$i"
+                                        python main_prediction.py --path_model_weights '../model_weights/model_weights_family_mother_machine/' --path_pos $PATH_FOLDER$POS --path_channel ${!CH} --postprocessing 1
+                                        python analyse_segmentation.py --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH/ --path_result $PATH_FOLDER$POS/${!CH}/
+                                done
+                        fi
+                fi
+
+                # 6) Conversion
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "SEGMENTATION" ]]
+                then
+                        echo "run file-conversion"
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                python seg2mat.py --path_cut $PATH_FOLDER$POS/${!CH}/$SEG_PATH$CUT_PATH --path_seg $PATH_FOLDER$POS/${!CH}/$SEG_IM_PATH --path_channel $PATH_FOLDER$POS/${!CH}/
+                        done
+                fi
+
+
+                # 7) Tracking
+                if [[ $RUN_OPTION == "BOTH" ]] || [[ $RUN_OPTION == "TRACKING" ]]
+                then
+                        echo "run tracking"
+                        
+                        for i in $(seq 1 $NUM_CHANNEL_TYPES); do
+                                CH="CHANNEL_$i"
+                                echo ${!CH}
+
+                                # delete all files related to SuperSegger to ensure that SuperSegger runs
                                 rm $PATH_FOLDER$POS/${!CH}/CONST.mat
+                                rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH/clist.mat
                                 rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH/*_err.mat
                                 rm -r $PATH_FOLDER$POS/${!CH}/$SEG_PATH/cell
                                 rm $PATH_FOLDER$POS/${!CH}/$RAW_IM/cropbox.mat
 
-                                python restrict_frames.py
-                                source settings.sh
-                                LIST_FILES=($(ls $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH))
-                                NUM_FILES=${#LIST_FILES[@]}
-                                NUM_REMOVE=$NUM_FILES-$END_FRAME #number of files to remove
-
-                                for FILE in ${LIST_FILES[@]:$END_FRAME:$NUM_REMOVE}; do
-                                        rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH/$FILE
-                                done
                                 $MATLAB_ROOT/bin/matlab -nodisplay -r "tracking_supersegger('$PATH_FOLDER$POS/${!CH}/', '$CONSTANTS' , $NEIGHBOR_FLAG, $TIME_STEP, $MIN_CELL_AGE, '$DATA_TYPE')"
-                        
-                        done
+                                MAT_FILE=$PATH_FOLDER$POS/${!CH}/$SEG_PATH/clist.mat
+                                echo $MAT_FILE
 
-                done
-        fi
+                                # as long as 'clist.mat' is missing (hint for failed SuperSegger) the tracking can be repeated with a reduced number of frames
+                                while ! test -f "$MAT_FILE"; do
+                                        #rm -r $PATH_FOLDER$POS/${!CH}/$SEG_PATH/
+                                        rm $PATH_FOLDER$POS/${!CH}/CONST.mat
+                                        rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH/*_err.mat
+                                        rm -r $PATH_FOLDER$POS/${!CH}/$SEG_PATH/cell
+                                        rm $PATH_FOLDER$POS/${!CH}/$RAW_IM/cropbox.mat
+
+                                        python restrict_frames.py
+                                        source settings.sh
+                                        LIST_FILES=($(ls $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH))
+                                        NUM_FILES=${#LIST_FILES[@]}
+                                        NUM_REMOVE=$NUM_FILES-$END_FRAME #number of files to remove
+
+                                        for FILE in ${LIST_FILES[@]:$END_FRAME:$NUM_REMOVE}; do
+                                                rm $PATH_FOLDER$POS/${!CH}/$SEG_PATH$SEG_MAT_PATH/$FILE
+                                        done
+                                        $MATLAB_ROOT/bin/matlab -nodisplay -r "tracking_supersegger('$PATH_FOLDER$POS/${!CH}/', '$CONSTANTS' , $NEIGHBOR_FLAG, $TIME_STEP, $MIN_CELL_AGE, '$DATA_TYPE')"
+                                
+                                done
+
+                        done
+                fi
+
 
     done
 fi
