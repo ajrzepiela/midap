25f7c1c73790c9b159857d241d1c7045366e588f
oschmanf <franziska.oschmann@id.ethz.ch>
2022-04-12 16:49:09 +0200

    Add command-line options to track_cells.py, track_cells_const.py, track_cells_crop.py
    Former-commit-id: dcf03563022625a05e7e2a76751d9e018a893460

--- bin/track_cells_const.py
+++ bin/track_cells_const.py
@@ -13,7 +13,7 @@ parser.add_argument('--num_frames', help='number of time frames')
 args = parser.parse_args()
 
 # Load data
-#args['path']: '../Example_For_Tracking-Co/1_5minCo/pos7/GFP/'
+#DeLTA_data = '../Example_For_Tracking-Co/1_5minCo/pos7/GFP/'
 images_folder = args.path + 'xy1/phase/'
 segmentation_folder = args.path + 'seg_im/'
 output_folder = args.path + 'track_output/'
@@ -24,12 +24,12 @@ seg_names_sort = np.sort(glob.glob(segmentation_folder + '*frame*'))[:int(args.n
 
 # Parameters:
 target_size = (512, 512)
-input_size = target_size + (4,)
+input_size = target_size + (1,)
 num_time_steps = len(img_names_sort)
 
 # Process
 tr = Tracking(img_names_sort, seg_names_sort, model_file, input_size, target_size)
-tr.track_all_frames()
+tr.track_all_frames_const()
 
 # Reduce results file for storage
 results_all_red = np.empty((len(tr.results_all), *tr.results_all[0][0,:,:,:2].shape))
@@ -39,5 +39,5 @@ for t in range(len(tr.results_all)):
         results_all_red[t,cell_id[:,:,1] > 0.5, 1] = ix+1
 
 # Save data
-np.savez(output_folder + 'inputs_all_red.npz', inputs_all=np.array(tr.inputs_all))
-np.savez(output_folder + 'results_all_red.npz', results_all_red=np.array(results_all_red))
+np.savez(output_folder + 'inputs_all_red_const.npz', inputs_all=np.array(tr.inputs_all))
+np.savez(output_folder + 'results_all_red_const.npz', results_all_red=np.array(results_all_red))
--- bin/track_cells_crop.py
+++ bin/track_cells_crop.py
@@ -13,7 +13,7 @@ parser.add_argument('--num_frames', help='number of time frames')
 args = parser.parse_args()
 
 # Load data
-#args['path']: '../Example_For_Tracking-Co/1_5minCo/pos7/GFP/'
+#DeLTA_data = '../Example_For_Tracking-Co/1_5minCo/pos7/GFP/'
 images_folder = args.path + 'xy1/phase/'
 segmentation_folder = args.path + 'seg_im/'
 output_folder = args.path + 'track_output/'
@@ -23,21 +23,23 @@ img_names_sort = np.sort(glob.glob(images_folder + '*frame*'))[:int(args.num_fra
 seg_names_sort = np.sort(glob.glob(segmentation_folder + '*frame*'))[:int(args.num_frames)]
 
 # Parameters:
-target_size = (512, 512)
-input_size = target_size + (4,)
+crop_size = (128,128)
+target_size = (512,512)
+input_size = crop_size + (4,)
 num_time_steps = len(img_names_sort)
 
 # Process
-tr = Tracking(img_names_sort, seg_names_sort, model_file, input_size, target_size)
-tr.track_all_frames()
+tr = Tracking(img_names_sort, seg_names_sort, model_file, input_size, target_size, crop_size)
+tr.track_all_frames_crop()
 
 # Reduce results file for storage
-results_all_red = np.empty((len(tr.results_all), *tr.results_all[0][0,:,:,:2].shape))
+results_all_red = np.zeros((len(tr.results_all), *tr.results_all[0][0,:,:,:2].shape))
+
 for t in range(len(tr.results_all)):
     for ix, cell_id in enumerate(tr.results_all[t]):
         results_all_red[t,cell_id[:,:,0] > 0.5, 0] = ix+1
         results_all_red[t,cell_id[:,:,1] > 0.5, 1] = ix+1
 
-# Save data
-np.savez(output_folder + 'inputs_all_red.npz', inputs_all=np.array(tr.inputs_all))
-np.savez(output_folder + 'results_all_red.npz', results_all_red=np.array(results_all_red))
+## Save data
+np.savez(output_folder + 'inputs_all_red_crop.npz', inputs_all=np.array(tr.inputs_all))
+np.savez(output_folder + 'results_all_red_crop.npz', results_all_red=np.array(results_all_red))
