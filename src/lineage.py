f56f266669c33ec29984050b3b115989555e5bbb
oschmanf <franziska.oschmann@id.ethz.ch>
2022-04-12 13:00:24 +0200

    Add documentation for lineage.py
    Former-commit-id: d9061904cc891015cee9c78238538a7d41e97128

--- src/lineage.py
+++ src/lineage.py
@@ -5,7 +5,7 @@ from scipy.spatial import distance
 
 class Lineages:
     """
-    A class to generate lienages based on trackinng outputs.
+    A class to generate lineages based on trackinng outputs.
 
     ...
 
@@ -43,48 +43,62 @@ class Lineages:
         self.results = results
 
     def generate_lineages(self):
+        """Generates lineages based on output of tracking (U-Net) network.
+
+        """
+        
+        # Generates global IDs for each frame and time step.
         self.generate_global_IDs()
 
+        # Generate empty label stack and dictionary to map global to unique IDs
         self.label_stack = np.empty((self.results.shape[:-1]))
-        num_time_steps = len(self.results)#-1
-
-        unique_ID = 0
-        
         self.global2uniqueID = {}
         self.label_dict = []
+
+        # Loops over all global IDs in list and connects cells between time frames
+        # with help of tracking results
+        num_time_steps = len(self.results)#-1
+        unique_ID = 0
         while len(self.global_IDs) > 0:
-            cell_dict = {}
             print(len(self.global_IDs))
+            
+            # Picks first entry from global IDs list
             start_ID = self.global_IDs[0]
+
+            # Per cell all relevant information are stored in dictionary and colledted in label_dict for all cells
             unique_ID += 1
+            cell_dict = {}
             cell_dict['ID'] = unique_ID
             cell_dict['frames'] = []
             cell_dict['mother'] = []
             cell_dict['daughters'] = []
             cell_dict['split'] = []
             
-            # get first time frame where cell is present
+            # Gets first time frame where cell is present
             first_frame_cell = np.where(self.global_label == start_ID)[0][0]
 
+            # Loops over all time frames 
             for frame_cell in range(first_frame_cell,num_time_steps):
                 cell_dict['frames'].append(frame_cell)
                 self.global2uniqueID[start_ID] = unique_ID
-                # get local ID within time frame
+
+                # Get local ID within time frame
                 local_ID = self.inputs[frame_cell,:,:,1][self.global_label[frame_cell].astype(int) == start_ID][0].astype(int)
 
                 mother = self.inputs[frame_cell,:,:,1] == local_ID
                 daughter_1 = self.results[frame_cell][:,:,0] == local_ID
                 daughter_2 = self.results[frame_cell][:,:,1] == local_ID
 
-                 # add new ID to label stack
+                 # Add mother with new (unique) ID to label stack
                 self.label_stack[frame_cell][self.inputs[frame_cell,:,:,1] == local_ID] = unique_ID
 
-                # for all cells which are not in the last time frame
+                # For all cells which are not in the last time frame
                 if frame_cell <= num_time_steps-2:
             
-                    # find local IDs in next time frame if cell still exists in next frame
+                    # Find global ID in next time frame to follow cell through time frames.
+                    # Only in case cell still exists in next frame.
 
-                    # case 1: only daughter 1 is present
+                    # Case 1: only daughter 1 is present
                     if daughter_1.sum() > 0 and daughter_2.sum() == 0:
                         new_global_ID = self.get_new_daughter_ID(daughter_1, frame_cell)
 
@@ -93,7 +107,7 @@ class Lineages:
                         
                         cell_dict['daughters'].append([new_global_ID])
 
-                    # case 2: only daughter 2 is present
+                    # Case 2: only daughter 2 is present
                     elif daughter_1.sum() == 0 and daughter_2.sum() > 0:
                         new_global_ID = self.get_new_daughter_ID(daughter_2, frame_cell)
 
@@ -102,7 +116,7 @@ class Lineages:
 
                         cell_dict['daughters'].append([new_global_ID])
 
-                    # case 3: cell split: both daughters are present
+                    # Case 3: cell split: both daughters are present
                     elif daughter_1.sum() > 0 and daughter_2.sum() > 0:
                         # get new ID for daughter one to continue tracking
                         new_global_ID_d1 = self.get_new_daughter_ID(daughter_1, frame_cell)
@@ -110,7 +124,9 @@ class Lineages:
                         
                         cell_dict['split'].append(frame_cell)
                         cell_dict['daughters'].append([new_global_ID_d1, new_global_ID_d2])
-
+                        
+                        # new_global_ID_d2 will stay in self.global_IDs and lineage generation is 
+                        # continued at later time point.
                         self.remove_global_ID(start_ID)
                         start_ID = new_global_ID_d1
                         
@@ -120,12 +136,14 @@ class Lineages:
 
                         cell_dict['daughters'].append([])
                         break
-        
+                
+                # Lineage generation is stopped in last time frame
                 if frame_cell > num_time_steps-2:
                     self.remove_global_ID(start_ID)
+
             self.label_dict.append(cell_dict)
             
-        # convert cell IDs
+        # convert cell IDs in label dict from global to unique IDs
         for ix, cell in enumerate(self.label_dict):
             ids = []
             for d in cell['daughters']:
@@ -134,8 +152,15 @@ class Lineages:
 
 
     def generate_global_IDs(self):
+        """Generates global IDs with one ID per cell and time frame and
+        list with all global IDs.
+
+        """
+
         self.global_label = np.empty(self.inputs[:,:,:,1].shape)
         
+        # Loops through all time frames and adding the maximal cell ID of the previous time frame to the 
+        # IDs/labels of the current time frame. By that every cell in every time frame gets one global ID. 
         max_val = 0
         for i, inp in enumerate(self.inputs[:,:,:,1]):
             new_label = inp + max_val
@@ -147,6 +172,17 @@ class Lineages:
 
 
     def get_new_daughter_ID(self, daughter, frame_cell):
+        """Extracts global ID of daughter cell in next time frame.
+        
+        Parameters
+        ----------
+        daughter: array
+            Segmentation image of daughter cell generated by the U-Net.
+
+        frame_cell: int
+            Number of the current time frame. 
+        """
+
         # get center of daughter in current frame
         daughter_cent = regionprops(daughter.astype(int))[0].centroid
 
@@ -164,9 +200,17 @@ class Lineages:
         return new_global_ID
 
 
-    def remove_global_ID(self, start_ID):
-        # remove start ID from global IDs
+    def remove_global_ID(self, current_ID):
+        """Removes global ID from list of all global IDs during the lineage generation.
+
+        Parameters
+        ----------
+        current_ID: int
+            ID which is currently processed during tracking.
+        """
+
+        # remove current ID from global IDs
         try:
-            self.global_IDs.remove(start_ID)
+            self.global_IDs.remove(current_ID)
         except ValueError:
             None
