64785c6cd000d5104cc8e38dbb22fe78ec9ac589
oschmanf <franziska.oschmann@id.ethz.ch>
2022-05-05 14:03:57 +0200

    Add argparse for command-line arguments (path to image and segmentation)
    Former-commit-id: b598bcec5e53e5188f155a41693c0caed48abf28

--- bin/correct_segmentation.py
+++ bin/correct_segmentation.py
@@ -4,26 +4,26 @@ Correct segmentations
 
 Correct a segmentation generated with Midap
 """
-import numpy as np
-from skimage import data
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
-from skimage.measure import label, regionprops_table
-from skimage.morphology import closing, square, remove_small_objects
+import argparse
 import skimage.io as io
 import napari
 from napari.settings import SETTINGS
 
 SETTINGS.application.ipy_interactive = False
 
+def load_data():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_img', help='path to image')
+    parser.add_argument('--path_seg', help='path to segmentation')
+    args = parser.parse_args()
+    return args
 
 def main():
-    path_img = '../../../ackermann-bacteria-segmentation/data/unet/family_machine/CB15-flgH/Caulobacter-flgH-Raw.tif'
-    path_seg = '../../../ackermann-bacteria-segmentation/data/unet/family_machine/CB15-flgH/Caulobacter-flgH-segmented.tif'
+    args = load_data()
 
     # load the image and segment it
-    image = io.imread(path_img)
-    label_image = io.imread(path_seg)
+    image = io.imread(args.path_img)
+    label_image = io.imread(args.path_seg)
 
     # initialise viewer with raw image and segmentation
     viewer = napari.Viewer()
@@ -35,8 +35,8 @@ def main():
     # save corrected segmentation image
     edited_labels = label_layer.data
 
-    name = '.'.join(path_seg.split('.')[:-1])
-    ext = path_seg.split('.')[-1]
+    name = '.'.join(args.path_seg.split('.')[:-1])
+    ext = args.path_seg.split('.')[-1]
     path_corr = name + '_corr.' + ext
     io.imsave(path_corr, edited_labels)
 
