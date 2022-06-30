e61533c01031720ba06594c5777c4143f2aebde0
oschmanf <franziska.oschmann@id.ethz.ch>
2022-06-30 15:20:27 +0200

    Add download of psf to download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -6,12 +6,20 @@ wget -O model_weights.tar https://polybox.ethz.ch/index.php/s/XoaFLr346h8GxzP/do
 echo "Download example data"
 wget -O example_data.tar https://polybox.ethz.ch/index.php/s/Ub30B0ivoTdGWzK/download
 
+echo "Download psf"
+wget -O psf.tar https://polybox.ethz.ch/index.php/s/a1oLGN73UNuxwQv
+
 echo "Extract model weights"
 tar -xvf model_weights.tar
 
 echo "Extract example data"
 tar -xvf example_data.tar
 
+echo "Extract psf"
+tar -xvf psf.tar
+
 echo "Remove tar-files"
 rm model_weights.tar
 rm example_data.tar
+rm psf.tar
+
