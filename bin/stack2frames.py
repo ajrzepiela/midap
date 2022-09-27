87011fc630b78fbcef11cb2f7baaa104b1e5bd52
Janis Fluri <janis.fluri@hotmail.com>
2022-09-27 09:20:42 +0200

    minor cleanup + logging

--- bin/stack2frames.py
+++ bin/stack2frames.py
@@ -1,4 +1,5 @@
 import argparse
+
 import os
 from tqdm import tqdm
 
@@ -6,33 +7,48 @@ from scipy.io import loadmat
 from skimage.restoration import richardson_lucy
 from skimage import io
 
+from utils import get_logger
+
+# argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument("--path")
-parser.add_argument("--pos")
-parser.add_argument("--channel")
-parser.add_argument("--start_frame")
-parser.add_argument("--end_frame")
-parser.add_argument("--deconv")
+parser.add_argument("--path", type=str, required=True, help="Path to the file of which the frames should be split")
+parser.add_argument("--start_frame", type=int, required=True, help="First frame to split off.")
+parser.add_argument("--end_frame", type=int, required=True, help="Last frame to split off.")
+parser.add_argument("--deconv", type=str, choices=["deconv_family_machine", "deconv_well", "no_deconv"],
+                    default="no_deconv", help="Deconvolution type that should be performed, defaults to no deconv.")
+parser.add_argument("--loglevel", type=int, default=7, help="Loglevel of the script.")
 args = parser.parse_args()
 
-path_head = os.path.split(args.path)[0]
-path_tail = os.path.split(args.path)[1]
+# logging
+logger = get_logger(__file__, args.loglevel)
+logger.info(f"Splitting frames of: {args.path}")
+
+# split the paths
+path_head, path_tail = os.path.split(args.path)
 raw_filename = os.path.splitext(path_tail)[0]
 
 # loop over tif/tiff-stack to extract single frames and deconvolve them if wanted
 if args.deconv == "deconv_family_machine":
-    psf = loadmat('../psf/PSFmme.mat')['PSF']
+    logger.debug("Running deconv for family machine.")
+    psf = loadmat(os.path.join('..', 'psf', 'PSFmme.mat'))['PSF']
     deconvolution = True
 elif args.deconv == "deconv_well":
-    psf = io.imread('../psf/PSF_BornWolf.tif')[5,:,:]
+    logger.debug("Running deconv for well.")
+    psf = io.imread(os.path.join('..', 'psf', 'PSF_BornWolf.tif'))[5,:,:]
     deconvolution = True
 else:
+    logger.debug("No deconv selected")
     deconvolution = False
+
+# split the frames
+logger.info("Splitting frames...")
 stack = io.imread(args.path)[int(args.start_frame):int(args.end_frame)]
 for ix, frame in enumerate(tqdm(stack)):
-        if deconvolution == True:
+        if deconvolution:
             deconvoluted = richardson_lucy(frame, psf, iterations=10, clip=False)
-            io.imsave(path_head + '/' + 'raw_im/' + raw_filename + '_frame' + str("%03d" % (ix + int(args.start_frame))) + '_deconv.png', deconvoluted, check_contrast=False)
+            io.imsave(os.path.join(path_head, 'raw_im', f'{raw_filename}_frame{ix + args.start_frame:03d}_deconv.png'),
+                      deconvoluted, check_contrast=False)
         else:
-            io.imsave(path_head + '/' + 'raw_im/' + raw_filename + '_frame' + str("%03d" % (ix + int(args.start_frame))) + '.png', frame, check_contrast=False)
+            io.imsave(os.path.join(path_head, 'raw_im', f'{raw_filename}_frame{ix + args.start_frame:03d}.png'),
+                      frame, check_contrast=False)
 
