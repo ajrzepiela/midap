9d4c2bbb9fe51ba955dea21ad6c28217d81a4203
Janis Fluri <janis.fluri@hotmail.com>
2022-11-29 17:11:58 +0100

    added tests for delta tracking

---
+++ test/tracking/test_base_tracking.py
@@ -0,0 +1,10 @@
+import pytest
+from midap.tracking.base_tracking import Tracking
+
+def test_base_cutout():
+    """
+    Tests the SegmentationPredictor abstract base class
+    """
+
+    with pytest.raises(TypeError):
+        _ = Tracking(imgs=None, segs=None, model_weights=None, input_size=None, target_size=None)
