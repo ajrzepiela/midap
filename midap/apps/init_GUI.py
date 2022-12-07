8236aa57a91524d29fb70a0521594cf25b74c821
Janis Fluri <janis.fluri@hotmail.com>
2022-12-07 03:11:57 +0100

    Refactored GUI, added config

--- midap/apps/init_GUI.py
+++ midap/apps/init_GUI.py
@@ -1,7 +1,10 @@
 import PySimpleGUI as sg
-from datetime import datetime
 import numpy as np
-import git
+
+from midap.config import Config
+
+# Get all subclasses for the dropdown menus
+###########################################
 
 # get all subclasses from the imcut
 from midap.imcut import *
@@ -21,178 +24,127 @@ from midap.tracking import base_tracking
 
 tracking_subclasses = [subclass.__name__ for subclass in base_tracking.Tracking.__subclasses__()]
 
-# set layout for GUI
-sg.theme('LightGrey1')
-appFont = ("Arial", 12)
-sg.set_options(font=appFont)
+# main function of the App
+##########################
 
-# Seclection of Family Machine vs Well
-layout = [[sg.Text('Data type', font='bold')],
-          [sg.Radio('Family/Mother Machine', "RADIO1", default=True, key="family_machine"),
-           sg.Radio('Well', "RADIO1", default=False, key="well")],
-          [sg.Text('')],
-          [sg.Column([[sg.OK(), sg.Cancel()]], key='col_final')]]
+def main():
+    """
+    The main function of the App
+    """
+    # set layout for GUI
+    sg.theme('LightGrey1')
+    appFont = ("Arial", 12)
+    sg.set_options(font=appFont)
 
-# Finalize layout
-window = sg.Window('Parameters', layout).Finalize()
+    # Common elements of the next GUI part
+    workflow = [[sg.Text('Part of pipeline', justification='center', size=(16, 1))],
+                [sg.T("         "), sg.Radio('Segmentation and Tracking', "RADIO1", default=True, key="segm_track")],
+                [sg.T("         "), sg.Radio('Segmentation', "RADIO1", default=False, key="segm_only")],
+                [sg.T("         "), sg.Radio('Tracking', "RADIO1", default=False, key="track_only")]]
 
-# read the values and close
-event, values = window.read()
-window.close()
-
-# Throw an error if we pressed cancel or X
-if event == 'Cancel' or event == None:
-    exit(1)
-
-# Common elements of the next GUI part
-workflow = [[sg.Text('Part of pipeline', justification='center', size=(16, 1))],
-            [sg.T("         "), sg.Radio('Segmentation and Tracking', "RADIO1", default=True, key="segm_track")],
-            [sg.T("         "), sg.Radio('Segmentation', "RADIO1", default=False, key="segm_only")],
-            [sg.T("         "), sg.Radio('Tracking', "RADIO1", default=False, key="track_only")]]
-
-frames = [[sg.Text('Set frame number')],
-          [sg.Input('0', size=(5, 30), key='start_frame'), sg.Text('-')],
-          [sg.Input('100', size=(5, 30), key='end_frame')]]
+    frames = [[sg.Text('Set frame number')],
+              [sg.Input('0', size=(5, 30), key='start_frame'), sg.Text('-')],
+              [sg.Input('100', size=(5, 30), key='end_frame')]]
 
 
-# open file handle for settings.sh
-with open("settings.sh", "w+") as file_settings:
-    # write date time header
-    current_time = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
-    file_settings.write(f"# Started run: {current_time}\n")
+    # Family Machine specific layout
+    layout_family_machine = [[sg.Frame('Conditional Run', [[
+        sg.Column(workflow, background_color='white'),
+        sg.Column(frames)
+    ]])],
+                             [sg.Text('Foldername', key='title_folder_name', font='bold')],
+                             [sg.Column([[sg.Input(key='folder_name'), sg.FolderBrowse()]], key='col_folder_name')],
+                             [sg.Text('')],
+                             [sg.Text('Filetype (e.g. tif, tiff, ...)', key='title_file_type', font='bold')],
+                             [sg.Input(key='file_type')],
+                             [sg.Text('Identifier of Position/Experiment (e.g. Pos, pos)', key='pos_id',
+                                      font='bold')],
+                             [sg.Input(key='pos')],
+                             [sg.Text('Identifier of phase channel (e.g. Phase, PH, ...)', key='phase_check',
+                                      font='bold')],
+                             [sg.Input(key='ch1'),
+                              sg.Checkbox('Segmentation/Tracking', key='phase_segmentation', font='bold')],
+                             [sg.Text('Comma separated list of identifiers of additional \n'
+                                      'channels (e.g. eGFP,GFP,YFP,mCheery,TXRED, ...)',
+                                      key='channel_1', font='bold')],
+                             [sg.Input(key='ch2')],
+                             [sg.Text('Select how the chamber cutout should be performed: ',
+                                      key='imcut_text', font='bold')],
+                             [sg.DropDown(key='imcut', values=imcut_subclasses, default_value="InteractiveCutout")],
+                             [sg.Text('Select how the cell segmentation should be performed: ',
+                                      key='seg_method_text', font='bold')],
+                             [sg.DropDown(key='seg_method', values=segmentation_subclasses,
+                                          default_value="UNetSegmentation")],
+                             [sg.Text('Select how the cell tracking should be performed: ',
+                                      key='track_method_text', font='bold')],
+                             [sg.DropDown(key='track_method', values=tracking_subclasses,
+                                          default_value="DeltaV2Tracking")],
+                             [sg.Text('')],
+                             [sg.Text('Preprocessing', font='bold')],
+                             [sg.Checkbox('Deconvolution of images', key='deconv', font='bold')],
+                             [sg.Text('')],
+                             [sg.Column([[sg.OK(), sg.Cancel()]], key='col_final')]]
 
-    # write the current git hash of the repo
-    repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.object.hexsha
-    file_settings.write(f"# Git hash: {sha}\n")
+    # Finalize the layout
+    window = sg.Window('Parameters', layout_family_machine).Finalize()
 
-    # go through cases
-    if values['family_machine'] == True:
-        # Family Machine specific layout
-        layout_family_machine = [[sg.Frame('Conditional Run', [[
-            sg.Column(workflow, background_color='white'),
-            sg.Column(frames)
-        ]])],
-                                 [sg.Text('Foldername', key='title_folder_name', font='bold')],
-                                 [sg.Column([[sg.Input(key='folder_name'), sg.FolderBrowse()]], key='col_folder_name')],
-                                 [sg.Text('')],
-                                 [sg.Text('Filetype (e.g. tif, tiff, ...)', key='title_file_type', font='bold')],
-                                 [sg.Input(key='file_type')],
-                                 [sg.Text('Identifier of Position/Experiment (e.g. Pos, pos)', key='pos_id',
-                                          font='bold')],
-                                 [sg.Input(key='pos')],
-                                 [sg.Text('Identifier of phase channel (e.g. Phase, PH, ...)', key='phase_check',
-                                          font='bold')],
-                                 [sg.Input(key='ch1'),
-                                  sg.Checkbox('Segmentation/Tracking', key='phase_segmentation', font='bold')],
-                                 [sg.Text('Comma separated list of identifiers of additional \n'
-                                          'channels (e.g. eGFP,GFP,YFP,mCheery,TXRED, ...)',
-                                          key='channel_1', font='bold')],
-                                 [sg.Input(key='ch2')],
-                                 [sg.Text('Select how the chamber cutout should be performed: ',
-                                          key='imcut_text', font='bold')],
-                                 [sg.DropDown(key='imcut', values=imcut_subclasses, default_value="InteractiveCutout")],
-                                 [sg.Text('Select how the cell segmentation should be performed: ',
-                                          key='seg_method_text', font='bold')],
-                                 [sg.DropDown(key='seg_method', values=segmentation_subclasses,
-                                              default_value="UNetSegmentation")],
-                                 [sg.Text('Select how the cell tracking should be performed: ',
-                                          key='track_method_text', font='bold')],
-                                 [sg.DropDown(key='track_method', values=tracking_subclasses,
-                                              default_value="DeltaV2Tracking")],
-                                 [sg.Text('')],
-                                 [sg.Text('Preprocessing', font='bold')],
-                                 [sg.Checkbox('Deconvolution of images', key='deconv', font='bold')],
-                                 [sg.Text('')],
-                                 [sg.Column([[sg.OK(), sg.Cancel()]], key='col_final')]]
+    # Read Params and close window
+    event, values = window.read()
+    window.close()
 
-        # Finalize the layout
-        window = sg.Window('Parameters', layout_family_machine).Finalize()
+    # Throw an error if we pressed cancel or X
+    if event == 'Cancel' or event == None:
+        # TODO: Think about what should happen here
+        exit(1)
 
-        # Read Params and close window
-        event, values = window.read()
-        window.close()
+    # Set the general parameter
+    general = {}
 
-        # Throw an error if we pressed cancel or X
-        if event == 'Cancel' or event == None:
-            exit(1)
+    # get all the channels
+    channels = values['ch1']
+    # Only an emtpy string is False
+    if values['ch2']:
+        channels += f",{values['ch2']}"
+    general["Channels"] = channels
 
-        # split the channel types
-        channel_type_vals = [values['ch1']]
-        # Only an emtpy string is False
-        if values['ch2']:
-            channel_type_vals += values['ch2'].split(",")
+    # Read out the Radio Button
+    run_options = ['both', 'segmentation', 'tracking']
+    cond_run = [values['segm_track'], values['segm_only'], values['track_only']]
+    ix_cond = np.where(np.array(cond_run))[0][0]
+    general["RunOption"] = run_options[ix_cond]
 
-        # Read out the Radio Button
-        run_options = ['BOTH', 'SEGMENTATION', 'TRACKING']
-        cond_run = [values['segm_track'], values['segm_only'], values['track_only']]
-        ix_cond = np.where(np.array(cond_run))[0][0]
+    # deconv
+    if values['deconv'] == True:
+        general["Deconvolution"] = "deconv_family_machine"
+    else:
+        general["Deconvolution"] = "no_deconv"
 
-        # Write the settings.sh
-        file_settings.write(f"RUN_OPTION={run_options[ix_cond]}\n")
-        if values['deconv'] == True:
-            file_settings.write("DECONVOLUTION=deconv_family_machine\n")
-        else:
-            file_settings.write("DECONVOLUTION=no_deconv\n")
-        file_settings.write(f"PHASE_SEGMENTATION={values['phase_segmentation']}\n")
-        file_settings.write(f"START_FRAME={values['start_frame']}\n")
-        file_settings.write(f"END_FRAME={values['end_frame']}\n")
-        file_settings.write(f"DATA_TYPE=FAMILY_MACHINE\n")
-        file_settings.write(f"PATH_FOLDER={values['folder_name']}/\n")
-        file_settings.write(f"FILE_TYPE={values['file_type']}\n")
-        file_settings.write(f"POS_IDENTIFIER={values['pos']}\n")
-        file_settings.write(f"CHAMBER_CUTOUT={values['imcut']}\n")
-        file_settings.write(f"SEGMENTATION_METHOD={values['seg_method']}\n")
-        file_settings.write(f"TRACKING_METHOD={values['track_method']}\n")
+    # The remaining generals
+    general["StartFrame"] = values['start_frame']
+    general["EndFrame"] = values['end_frame']
+    general["DataType"] = "Family_Machine"
+    general["FolderPath"] = values['folder_name']
+    general["PosIdentifier"] = values['pos']
+    general["FileType"] = values['file_type']
+    general["PhaseSegmentation"] = values['phase_segmentation']
 
-        for i, s in enumerate(channel_type_vals):
-            file_settings.write(f"CHANNEL_{i + 1}={s}\n")
-        file_settings.write(f"NUM_CHANNEL_TYPES={len(channel_type_vals)}\n")
+    # the image cut method
+    cut_img = {"Class": values['imcut']}
 
-    elif values['well'] == True:
-        # Well specific layout
-        layout_well = [[sg.Frame('Conditional Run', [[
-            sg.Column(workflow, background_color='white'),
-            sg.Column(frames)
-        ]])],
-                       [sg.Text('Filename', key='title_file_name', font='bold')],
-                       [sg.Column([[sg.Input(key='file_name'), sg.FileBrowse()]], key='col_file_name')],
-                       [sg.Text('')],
-                       [sg.Text('Filetype (e.g. tif, tiff, ...)', key='title_file_type', font='bold')],
-                       [sg.Input(key='file_type')],
-                       [sg.Text('Identifier of Position/Experiment (e.g. Pos, pos)', key='pos_id', font='bold')],
-                       [sg.Input(key='pos')],
-                       [sg.Text('')],
-                       [sg.Text('Preprocessing', font='bold')],
-                       [sg.Checkbox('Deconvolution of images', key='deconv', font='bold')],
-                       [sg.Text('')],
-                       [sg.Column([[sg.OK(), sg.Cancel()]], key='col_final')]]
+    # the segmentation method
+    segmentation = {"Class": values['seg_method']}
 
-        # Finalize the layout
-        window = sg.Window('Parameters', layout_well).Finalize()
+    # the tracking method
+    tracking = {"Class": values['track_method']}
 
-        # Read params and close window
-        event, values = window.read()
-        window.close()
+    # init the config
+    config = Config(general=general, segmentation=segmentation, cut_img=cut_img, tracking=tracking)
 
-        # Throw an error if we pressed cancel or X
-        if event == 'Cancel' or event == None:
-            exit(1)
+    # write to file
+    config.to_file("settings.ini", overwrite=True)
 
-        # Read out the radio button
-        run_options = ['BOTH', 'SEGMENTATION', 'TRACKING']
-        cond_run = [values['segm_track'], values['segm_only'], values['track_only']]
-        ix_cond = np.where(np.array(cond_run))[0][0]
+# Run as script
+###############
 
-        # write the settings.sh
-        file_settings.write(f"RUN_OPTION={run_options[ix_cond]}\n")
-        if values['deconv'] == True:
-            file_settings.write("DECONVOLUTION=deconv_well\n")
-        else:
-            file_settings.write("DECONVOLUTION=no_deconv\n")
-        file_settings.write(f"START_FRAME={values['start_frame']}\n")
-        file_settings.write(f"END_FRAME={values['end_frame']}\n")
-        file_settings.write(f"DATA_TYPE=WELL\n")
-        file_settings.write(f"PATH_FILE={values['file_name']}\n")
-        file_settings.write(f"FILE_TYPE={values['file_type']}\n")
-        file_settings.write(f"POS_IDENTIFIER={values['pos']}\n")
+if __name__ == "__main__":
+    main()
--- midap/config.py
+++ midap/config.py
@@ -1,5 +1,30 @@
+import git
+
 from configparser import ConfigParser
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
+
+# Get all subclasses to check validity of config
+################################################
+
+# get all subclasses from the imcut
+from midap.imcut import *
+from midap.imcut import base_cutout
+
+imcut_subclasses = [subclass.__name__ for subclass in base_cutout.CutoutImage.__subclasses__()]
+
+# get all subclasses from the segmentations
+from midap.segmentation import *
+from midap.segmentation import base_segmentator
+
+segmentation_subclasses = [subclass.__name__ for subclass in base_segmentator.SegmentationPredictor.__subclasses__()]
+
+# get all subclasses from the tracking
+from midap.tracking import *
+from midap.tracking import base_tracking
+
+tracking_subclasses = [subclass.__name__ for subclass in base_tracking.Tracking.__subclasses__()]
 
 class Config(ConfigParser):
     """
@@ -20,6 +45,9 @@ class Config(ConfigParser):
         # init the parser
         super().__init__()
 
+        # make all keys case sensitive
+        self.optionxform = str
+
         # set the defaults
         self.set_defaults()
 
@@ -32,17 +60,107 @@ class Config(ConfigParser):
         Sets all values of the Config to the default values
         """
 
-        self.read_dict({"General": {"RunOption": "both",
-                                    "Deconvolution": "no_deconf",
+        # get the SHA of the git repo
+        try:
+            repo = git.Repo(path=Path(__file__).parent, search_parent_directories=True)
+            sha = repo.head.object.hexsha
+        except git.InvalidGitRepositoryError:
+            sha = 'None'
+
+        # set defaults
+        self.read_dict({"General": {"Timestamp": datetime.now().strftime("%Y-%m-%d, %H:%M:%S"),
+                                    "Git hash": sha,
+                                    "RunOption": "both",
+                                    "Deconvolution": "no_deconv",
                                     "StartFrame": 0,
                                     "EndFrame": 10,
                                     "DataType": "Family_Machine",
                                     "FolderPath": "None",
                                     "FileType": "tif",
                                     "PosIdentifier": "pos",
+                                    "PhaseSegmentation": False,
                                     "Channels": 'None'
                                     },
-                        "CutImg": {},
-                        "Segmentation": {},
-                        "Tracking": {}})
+                        "CutImg": {"Class": "InteractiveCutout"},
+                        "Segmentation": {"Class": "UNetSegmentation"},
+                        "Tracking": {"Class": "DeltaV2Tracking"}})
 
+    def validate_sections(self):
+        """
+        Validates the content of all sections in choice and type
+        :raises: ValueError if invalid value is found or other Errors accordingly
+        """
+
+        # run option choices
+        allowed_run_options = ['both', 'segmentation', 'tracking']
+        if self.get("General", "RunOption").lower() not in allowed_run_options:
+            raise ValueError(f"'RunOption' not in {allowed_run_options}")
+
+        # deconvolution choices
+        allowed_deconv = ["deconv_family_machine", "no_deconv"]
+        if self.get("General", "Deconvolution").lower() not in allowed_deconv:
+            raise ValueError(f"'Deconvolution' not in {allowed_deconv}")
+
+        # check the DataType
+        allowed_datatype = ["Family_Machine"]
+        if self.get("General", "DataType") not in allowed_datatype:
+            raise ValueError(f"'DataType' not in {allowed_datatype}")
+
+        # check the ints
+        if (start_frame := self.getint("General", "StartFrame")) < 0:
+            raise ValueError(f"'StartFrame' has to be a positive integer, is: {start_frame}")
+        if (end_frame := self.getint("General", "EndFrame")) < 0 or end_frame <= start_frame:
+            raise ValueError(f"'EndFrame' has to be a positive integer and larger than 'StartFrame', is: {start_frame}")
+
+        # check the booleans
+        _ = self.getboolean("General", "PhaseSegmentation")
+
+        # check the paths
+        if not (folder_path := Path(self.get("General", "FolderPath"))).exists():
+            raise FileNotFoundError(f"'FolderPath' not an existing directory: {folder_path}")
+
+        # check all the classes
+        if self.get("CutImg", "Class") not in imcut_subclasses:
+            raise ValueError(f"'Class' of 'CutImg' not in {imcut_subclasses}")
+        if self.get("Segmentation", "Class") not in segmentation_subclasses:
+            raise ValueError(f"'Class' of 'Segmentation' not in {segmentation_subclasses}")
+        if self.get("Tracking", "Class") not in tracking_subclasses:
+            raise ValueError(f"'Class' of 'Tracking' not in {tracking_subclasses}")
+
+    def to_file(self, fname, overwrite=True):
+        """
+        Write the config into a file
+        :param fname: Name of the file to write
+        :param overwrite: Overwrite existing file, defaults to True
+        :raises: FileExistsError if overwrite is False and file exists
+        """
+
+        # check
+        if not overwrite and Path(fname).exists():
+            FileExistsError(f"File already exists, set overwrite to True to overwrite: {fname}")
+
+        # now we can open a w+ without worrying
+        with open(fname, "w+") as f:
+            self.write(f)
+
+    @classmethod
+    def from_file(cls, fname):
+        """
+        Initiates a new instance of the class and overwrites the defaults with contents from a file. The contents read
+        from the file will be checked for validity.
+        :param fname: The name of the file to read
+        :return: An instance of the class
+        """
+
+        # create a class instance
+        config = Config()
+
+        # read the file
+        with open(fname, "r") as f:
+            config.read_file(f)
+
+        # check validity
+        config.validate_sections()
+
+        # if no error was thrown we return the instance
+        return config
