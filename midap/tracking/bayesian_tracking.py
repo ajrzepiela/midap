10cd995eb4be41cbed8037714eb905ef7cf8ff11
oschmanf <franziska.oschmann@id.ethz.ch>
2022-12-08 20:58:56 +0100

    Add ccorrection of dataframe

--- midap/tracking/bayesian_tracking.py
+++ midap/tracking/bayesian_tracking.py
@@ -40,6 +40,8 @@ class BayesianCellTracking(Tracking):
                             "intensity_mean",
                             "intensity_min",
                             "intensity_max",
+                            "minor_axis_length",
+                            "major_axis_length",
                             "coords"]
 
         self.objects = btrack.utils.segmentation_to_objects(
@@ -60,6 +62,7 @@ class BayesianCellTracking(Tracking):
         self.run_model()
         self.convert_data()
         self.generate_label_stack()
+        self.correct_label_stack()
         self.store_lineages(*args, **kwargs)
 
 
@@ -121,10 +124,11 @@ class BayesianCellTracking(Tracking):
                     self.label_stack[t][tr['coords'][i][:,0],tr['coords'][i][:,1]] = tr['ID']
                     
                 except IndexError:
-                    point = (int(tr['y'][i]), int(tr['x'][i]))
-                    ix_cell = self.__find_nearest_neighbour(point, self.seg_imgs[t])
-                    self.label_stack[t][ix_cell] = tr['ID']
-                    print(tr['ID'], i)
+                    None
+                    # point = (int(tr['x'][i]), int(tr['y'][i]))
+                    # ix_cell = self.__find_nearest_neighbour(point, self.seg_imgs[t])
+                    # self.label_stack[t][ix_cell] = tr['ID']
+                    # print(tr['ID'], i)
 
 
     def __find_nearest_neighbour(self, point: tuple, seg: np.ndarray):
@@ -136,9 +140,67 @@ class BayesianCellTracking(Tracking):
 
         centroids = [r.centroid for r in regionprops(seg)]
         labels = [r.label for r in regionprops(seg)]
-        ix_min = np.argmin([distance.euclidean(c, point) for c in centroids])
+        ix_min = np.argsort([distance.euclidean(c, point) for c in centroids])[1]
         ix_cell = np.where(seg == labels[ix_min])
-        return ix_cell
+        return ix_min
+
+
+    def correct_label_stack(self):
+
+        for t in range(1,len(self.label_stack)):
+
+            labels_prev_frame = np.unique(self.label_stack[t-1])
+            labels_cur_frame = np.unique(self.label_stack[t])
+
+            # find new IDs
+            diff_ix = np.array([l2 not in labels_prev_frame for l2 in labels_cur_frame])
+
+            # find labels and centroids of new cells
+            reg = regionprops((self.label_stack[t]).astype(int))
+            centroids = [r.centroid for r in reg]
+            labels = [r.label for r in reg]
+            new_cells = labels_cur_frame[diff_ix]
+
+            # find closest cell and do correction of label stack
+            self.label_stack_correct = self.label_stack 
+            self.track_output_correct = self.track_output.copy()
+            for c in new_cells:
+                ix = np.where(labels == c)[0][0]
+                ix_closest_cell = self.__find_nearest_neighbour(centroids[ix], self.label_stack[t].astype(int))
+                new_ID_d1 = labels[ix]
+                new_ID_d2 = self.__new_ID()
+                old_ID_d1 = labels[ix_closest_cell]
+
+                self.label_stack_correct[t:][self.label_stack[t:] == old_ID_d1] = new_ID_d2
+
+                # import pdb
+                # pdb.set_trace()
+
+                # update df
+                for t_tmp_1 in range(1,t):
+                    self.track_output.at[t_tmp_1,'trackID_d1'][self.track_output.at[t_tmp_1,'trackID'] == old_ID_d1] = new_ID_d1 #set new trackID
+                    self.track_output.at[t_tmp_1,'trackID_d2'][self.track_output.at[t_tmp_1,'trackID'] == old_ID_d1] = new_ID_d2 #set new trackID
+                    self.track_output.at[t_tmp_1,'split_frame'][self.track_output.at[t_tmp_1,'trackID'] == old_ID_d1] = t #set new trackID
+                for t_tmp_2 in range(t,len(self.label_stack)):
+                    # import pdb
+                    # pdb.set_trace()
+                    self.track_output.loc[self.track_output.trackID == old_ID_d1].loc[t_tmp_2] = 111
+                    #self.track_output.loc[t_tmp_2].at['trackID'== old_ID_d1] = 111
+                    #self.track_output_correct.at[t_tmp_2,'trackID'][self.track_output.at[t_tmp_2,'trackID'] == old_ID_d1] = 111#new_ID_d2 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_mother'][self.track_output.at[t_tmp_2,'trackID'] == old_ID_d1] = old_ID_d1 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_d1'][self.track_output.at[t_tmp_2,'trackID'] == old_ID_d1] = new_ID_d1 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_d2'][self.track_output.at[t_tmp_2,'trackID'] == old_ID_d1] = new_ID_d1 #set new trackID
+                    
+                    # self.track_output_correct.at[t_tmp_2,'trackID'][self.track_output.at[t_tmp_2,'trackID'] == old_ID_d1] = new_ID_d2 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_mother'][self.track_output.at[t_tmp_2,'trackID'] == new_ID_d1] = old_ID_d1 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_d1'][self.track_output.at[t_tmp_2,'trackID'] == new_ID_d1] = new_ID_d1 #set new trackID
+                    # self.track_output_correct.at[t_tmp_2,'trackID_d2'][self.track_output.at[t_tmp_2,'trackID'] == new_ID_d1] = new_ID_d1 #set new trackID
+
+
+
+    def __new_ID(self):
+        return np.max(self.label_stack_correct)+1
+
 
     def convert_data(self):
         """
@@ -156,6 +218,8 @@ class BayesianCellTracking(Tracking):
         intensity_mean = []
         intensity_min = []
         intensity_max = []
+        minor_axis_length = []
+        major_axis_length = []
         first_frame = []
         last_frame = []
 
@@ -170,6 +234,8 @@ class BayesianCellTracking(Tracking):
             intensity_mean.append(t['intensity_mean'])
             intensity_min.append(t['intensity_min'])
             intensity_max.append(t['intensity_max'])
+            minor_axis_length.append(t['minor_axis_length'])
+            major_axis_length.append(t['major_axis_length'])
             first_frame.append([t.t[0]]*len(t.t))
             last_frame.append([t.t[-1]]*len(t.t))
                 
@@ -191,6 +257,8 @@ class BayesianCellTracking(Tracking):
         intensity_mean = np.concatenate(intensity_mean)
         intensity_min = np.concatenate(intensity_min)
         intensity_max = np.concatenate(intensity_max)
+        minor_axis_length = np.concatenate(minor_axis_length)
+        major_axis_length = np.concatenate(major_axis_length)
         first_frame = np.concatenate(first_frame)
         last_frame = np.concatenate(last_frame)
 
@@ -199,12 +267,15 @@ class BayesianCellTracking(Tracking):
                                 'trackID_d1':trackID_d1, 
                                 'trackID_d2':trackID_d2, 
                                 'trackID_mother':trackID_mother, 
+                                'split_frame':np.zeros(len(time)),
                                 'area':area, 
                                 'x':x_coor, 
                                 'y':y_coor,
                                 'intensity_mean':intensity_mean,
                                 'intensity_min':intensity_min,
                                 'intensity_max':intensity_max,
+                                'minor_axis_length':minor_axis_length,
+                                'major_axis_length':major_axis_length,
                                 'first_frame':first_frame, 
                                 'last_frame':last_frame})
 
@@ -219,8 +290,12 @@ class BayesianCellTracking(Tracking):
                                                                 'intensity_mean':'first',
                                                                 'intensity_min':'first',
                                                                 'intensity_max':'first',
+                                                                'minor_axis_length':'first',
+                                                                'major_axis_length':'first',
                                                                 'first_frame':'first',
                                                                 'last_frame':'first'}).reindex(columns=df_conv.columns)
+
+        self.track_output.set_index('frame', inplace=True)
 
 
     def store_lineages(self, logger, output_folder: str):
@@ -230,7 +305,7 @@ class BayesianCellTracking(Tracking):
         :output_folder: Folder where to store the data
         """
 
-        self.track_output.to_csv(output_folder + '/track_output_bayesian.csv', index=True)
+        self.track_output_correct.to_csv(output_folder + '/track_output_bayesian.csv', index=True)
 
         hf = h5py.File(output_folder + '/raw_inputs_bayesian.h5', 'w')
         raw_inputs = self.raw_imgs
@@ -243,5 +318,5 @@ class BayesianCellTracking(Tracking):
         hf.close()
 
         hf = h5py.File(output_folder + '/label_stack_bayesian.h5', 'w')
-        hf.create_dataset('label_stack', data=self.label_stack)
+        hf.create_dataset('label_stack', data=self.label_stack_correct)
         hf.close()
