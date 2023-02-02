256be4c2fcbdffa77bbbb3dcfce494cc9c6d6503
Janis Fluri <jafluri@cdss-m1-mini.ethz.ch>
2023-02-02 12:42:44 +0100

    added miniconda install and swtich to zsh

--- gpu_install/mac_arm_conda/install_midap_gpu.sh
+++ gpu_install/mac_arm_conda/install_midap_gpu.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/bin/zsh
+
+# check if we have miniconda
+if [[ ! -d ~/miniconda ]]; then
+  while true; do
+    read -q "yn?Miniconda seems missing, do you want to install it? Y/N "
+    case $yn in
+        [Yy]* ) wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh && bash miniconda.sh -b -p $HOME/miniconda && rm miniconda.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no. ";;
+    esac
+  done
+fi
 
 
 # update conda and environment
+source ~/miniconda/bin/activate
 conda env update -f environment_gpu.yml
 
 # activate environment
-source ~/miniconda3/etc/profile.d/conda.sh
 conda activate midap_gpu
 
 # instal midap
