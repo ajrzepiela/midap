9c56c7d01c111ab032b03452760d11b5cc71ce38
Franziska Oschmann <franziskaoschmann@Franziskas-MBP.fritz.box>
2023-07-19 13:41:39 +0200

    Remove unsued code

--- midap/apps/track_analysis.py
+++ midap/apps/track_analysis.py
@@ -16,44 +16,6 @@ def open_h5(h5_file: Union[str,os.PathLike]) -> Tuple[np.ndarray, np.ndarray]:
     labels = np.array(f['labels'])
     return images, labels
 
-def add_labels(labels_ph: np.ndarray, labels_gfp: np.ndarray, labels_mcherry: np.ndarray) -> np.ndarray:
-    """
-    Superimposes labels
-    """
-    added_labels = (labels_ph > 0).astype(int)
-    added_labels += 2*(labels_gfp > 0).astype(int)
-    added_labels += 4*(labels_mcherry > 0).astype(int)
-
-    return added_labels
-
-def save_added_labels(added_labels):
-    """
-    Saves added labels as h5 file
-    """
-    path = '../../../midap-datasets/Simon-GlucoseAcetate/GlucoseAcetateExampleData/Data/pos1/ph/track_output/'
-    hf = h5py.File(path+'tracking_postprocessed.h5', 'w')
-    hf.create_dataset('added_labels', data=added_labels)
-    hf.close()
-    # fig, ax = plt.subplots()
-    # cax = ax.imshow(added_labels[10])
-    # cbar = fig.colorbar(cax, ticks=[0, 1, 2, 3, 4, 5, 4, 6, 7])
-    # # cbar.ax.set_yticklabels(['BG', 'PH', 'GFP', 'mCherry', 'PH+GFP', 'PH+mCherry' + 'GFP+mCherry', 'PH+GFP+mCherry'])
-
-    # # fig.savefig('../../../midap-datasets/Simon-GlucoseAcetate/GlucoseAcetateExampleData/Data/pos1/ph/track_output/added_labels.png')
-
-def get_num2fluo():
-    num2fluo = dict()
-
-    num2fluo[1] = ['ph']
-    num2fluo[2] = ['gfp']
-    num2fluo[4] = ['mcherry']
-    num2fluo[3] = ['ph','gfp']
-    num2fluo[5] = ['ph','mcherry']
-    num2fluo[6] = ['gfp','mcherry']
-    num2fluo[7] = ['ph','gfp','mcherry']
-
-    return num2fluo
-
 def main(path, channels, tracking_class):
     # generate pathnames
     filename_h5 = 'tracking_' + tracking_class.lower() + '.h5'
@@ -97,7 +59,7 @@ def main(path, channels, tracking_class):
         
 
 if __name__ == "__main__":
-    path = PosixPath('/Users/franziskaoschmann/Documents/midap-datasets/Simon-GlucoseAcetate/GlucoseAcetateExampleData/Data/pos1')
+    path = Path('/Users/franziskaoschmann/Documents/midap-datasets/Simon-GlucoseAcetate/GlucoseAcetateExampleData/Data/pos1')
     channels = ['ph', 'gfp', 'mCherry']
     tracking_class = 'STrack'
     main(path, channels, tracking_class=tracking_class)
