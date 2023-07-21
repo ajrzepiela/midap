9f1fde4ee5560c0c926c8571ee9ab9d43c29c19a
oschmanf <franziska.oschmann@id.ethz.ch>
2023-07-21 10:07:42 +0200

    First commit: notebooks for training on JupyterHub

---
+++ training/notebook_jupyterhub/run_training.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module purge
+module load gcc/8.2.0 python_gpu/3.10.4 hdf5/1.10.1 eth_proxy
+source ../../euler/midap/bin/activate
+
+sbatch --mem-per-cpu=12g --gpus=1 --gres=gpumem:12g --time=01:00:00 --mail-type=START,END,FAIL --wrap "python ../train.py --n_grid 2 --epochs 10 --save_path weights data/*raw.tif"
+
