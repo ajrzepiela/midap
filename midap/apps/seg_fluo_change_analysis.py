139dc1ed84483eb558e8e9f6c7b299a0a4f8c282
Franziska Oschmann <franziskaoschmann@Franziskas-MBP.fritz.box>
2024-02-28 09:17:35 +0100

    Adapt analysis for unknown number of additional channels

--- midap/apps/seg_fluo_change_analysis.py
+++ midap/apps/seg_fluo_change_analysis.py
@@ -21,7 +21,7 @@ def load_img_stack(path: Union[str, os.PathLike], files: List[Union[str, os.Path
     stack = np.array(stack)
     return stack
 
-def main(path: Union[str, os.PathLike], channels: List[str]):
+def fluo_analysis_per_channel(path: Union[str, os.PathLike], ref_channel: str, add_channel: str):
     """
     Loads tracking output and adds intensities per cell and channel to dataframe.
     :param path: Path to output folder.
@@ -29,74 +29,80 @@ def main(path: Union[str, os.PathLike], channels: List[str]):
     :param tracking_class: Name of used tracking class.
     """
 
-    path_ph_channel = Path(path).joinpath(channels[0])
-    path_ch1_channel = Path(path).joinpath(channels[1])
-    path_ch2_channel = Path(path).joinpath(channels[2])
+    # create path's
+    path_ref_channel = Path(path).joinpath(ref_channel)
+    path_add_channel = Path(path).joinpath(add_channel)
 
-    path_ph_seg = path_ph_channel.joinpath('seg_im')
-    path_ch1_img = path_ch1_channel.joinpath('cut_im')
-    path_ch2_img = path_ch2_channel.joinpath('cut_im')
-    path_ch1_img_raw = path_ch1_channel.joinpath('cut_im_rawcounts')
-    path_ch2_img_raw = path_ch2_channel.joinpath('cut_im_rawcounts')
+    path_ref_ch_seg = path_ref_channel.joinpath('seg_im')
+    path_add_ch_img = path_add_channel.joinpath('cut_im')
+    path_add_ch_img_raw = path_add_channel.joinpath('cut_im_rawcounts')
 
-    ph_seg_all_files = np.sort(os.listdir(path_ph_seg))
-    ch1_img_all_files = np.sort(os.listdir(path_ch1_img))
-    ch2_img_all_files = np.sort(os.listdir(path_ch2_img))
+    # get all file names from folder
+    ref_ch_seg_all_files = np.sort(os.listdir(path_ref_ch_seg))
+    add_ch_img_all_files = np.sort(os.listdir(path_add_ch_img))
+    add_ch_img_raw_all_files = np.sort(os.listdir(path_add_ch_img_raw))
 
-    ch1_img_raw_all_files = np.sort(os.listdir(path_ch1_img_raw))
-    ch2_img_raw_all_files = np.sort(os.listdir(path_ch2_img_raw))
-
-    segs_ph = load_img_stack(path_ph_seg, ph_seg_all_files)
-    img_ch1 = load_img_stack(path_ch1_img, ch1_img_all_files)
-    img_ch2 = load_img_stack(path_ch2_img, ch2_img_all_files)
-    img_ch1_raw = load_img_stack(path_ch1_img_raw, ch1_img_raw_all_files)
-    img_ch2_raw = load_img_stack(path_ch2_img_raw, ch2_img_raw_all_files)
-
+    # load img stacks
+    segs_ref_ch = load_img_stack(path_ref_ch_seg, ref_ch_seg_all_files)
+    img_add_ch = load_img_stack(path_add_ch_img, add_ch_img_all_files)
+    img_add_ch_raw = load_img_stack(path_add_ch_img_raw, add_ch_img_raw_all_files)
 
     # Loop through all frames
     df_all = pd.DataFrame()
 
-    for frame in tqdm(range(len(segs_ph))):
-        ph = segs_ph[frame]
-        ch1 = img_ch1[frame]
-        ch2 = img_ch2[frame]
-        ch1_raw = img_ch1_raw[frame]
-        ch2_raw = img_ch2_raw[frame]
+    for frame in tqdm(range(len(segs_ref_ch))):
+        ref_ch = segs_ref_ch[frame]
+        add_ch = img_add_ch[frame]
+        add_ch_raw = img_add_ch_raw[frame]
 
-        props = regionprops_table(ph, properties=('label', 'coords'))
+        props = regionprops_table(ref_ch, properties=('label', 'coords'))
     
         df = pd.DataFrame(props)
 
         df = df.set_index('label')
 
-        intensities_ch1 = []
-        intensities_ch2 = []
-        intensities_raw_ch1 = []
-        intensities_raw_ch2 = []
+        intensities_add_ch = []
+        intensities_raw_add_ch = []
 
         # Loop through all cells of frame
         for i in df.index:
-            intensities_ch1.append(np.mean(ch1[df.loc[i].coords]))
-            intensities_ch2.append(np.mean(ch2[df.loc[i].coords]))
-            intensities_raw_ch1.append(np.mean(ch1_raw[df.loc[i].coords]))
-            intensities_raw_ch2.append(np.mean(ch2_raw[df.loc[i].coords]))
+            intensities_add_ch.append(np.mean(add_ch[df.loc[i].coords]))
+            intensities_raw_add_ch.append(np.mean(add_ch_raw[df.loc[i].coords]))
 
-        df['intensity_'+channels[1]] = intensities_ch1
-        df['intensity_'+channels[2]] = intensities_ch2
-        df['intensity_raw_'+channels[1]] = intensities_raw_ch1
-        df['intensity_raw_'+channels[2]] = intensities_raw_ch2
+        df['intensity_'+add_channel] = intensities_add_ch
+        df['intensity_raw_'+add_channel] = intensities_raw_add_ch
         df['frame_number'] = [frame]*len(df.index)
         df.drop(['coords'], axis=1, inplace=True)
 
         df_all = pd.concat([df_all, df])
 
-    df_all.to_csv(path_ph_channel.joinpath('fluo_intensities.csv'))
+    return df_all
+    
+
+def main(path: Union[str, os.PathLike], channels: List[str]):
+    """
+    Loads tracking output and adds intensities per cell and channel to dataframe.
+    :param path: Path to output folder.
+    :param channels: List with channels.
+    :param tracking_class: Name of used tracking class.
+    """
+    add_channels = channels[1:]
+
+    df_all_channels = pd.DataFrame()
+    for add_channel in add_channels:
+        df = fluo_analysis_per_channel(path=path, ref_channel=channels[0], add_channel=add_channel)
+        df_all_channels = pd.concat([df_all_channels, df], axis=1)
+
+    df_all_channels = df_all_channels.loc[:,~df_all_channels.columns.duplicated()].copy()
+
+    path_ref_channel = Path(path).joinpath(channels[0])
+    df_all_channels.to_csv(path_ref_channel.joinpath('fluo_intensities.csv'))
 
 
 if __name__ == "__main__":
     path = Path(
         "/Users/franziskaoschmann/Documents/midap-datasets/Simon-GlucoseAcetate/GlucoseAcetateExampleData/Data/pos1"
     )
-    channels = ["ph", "gfp", "mCherry"]
+    channels = ["ph", "gfp"]
     tracking_class = "STrack"
     main(path, channels)
