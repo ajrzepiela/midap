35b0155be7f55375b374b4616d50cdc97b98e564
Franziska Oschmann <franziska.oschmann@id.ethz.ch>
2024-11-26 10:16:58 +0100

    Update environment.yml

---
+++ gpu_install/mac_arm_conda/install_midap_gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# check if we have miniconda
+if [ ! -d ~/miniconda ] && [ ! -d ~/miniconda3 ]; then
+  while true; do
+    read -q "yn?Miniconda seems missing, do you want to install it? Y/N "
+    case $yn in
+        [Yy]* ) wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh && bash miniconda.sh -b -p $HOME/miniconda && rm miniconda.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no. ";;
+    esac
+  done
+fi
+
+
+# update conda and environment
+source ~/miniconda/bin/activate
+conda env update -f environment_gpu.yml
+
+# activate environment
+conda activate midap_gpu
+
+# instal midap
+python3 -m pip install -e ../..
